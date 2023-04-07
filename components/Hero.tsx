45474ee8dc2c9e0c4312a1a378e37278c36dcc3c
Martin Andersson <martin.andersson@me.com>
2023-04-07 20:06:43 +0200

    circles and photo added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from './BackgroundCircles';
 
 type Props = {};
 
 export default function Hero({}: Props) {
     const [text, count] = useTypewriter({
         words: [
-            "Hi, The Name's Martin Andersson",
-            "Guy-who-loves-Sailng",
-            "<ButLoveToCodeMore />"
+            "Hi, the Name is Martin Andersson",
+            "Guy-who-loves-Tech",
+            "<AndLovesToCodeMore />"
         ],
         loop: true,
         delaySpeed: 2000
     });
-  return <div>
-    <p>{text}</p>
-  </div>
+  return (
+    <div
+      className="h-screen flex flex-col space-y-8 items-center justify-center
+    text-center overflow-hidden"
+    >
+      <BackgroundCircles />
+      <img
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="/Foto.jpg"
+        alt=""
+      />
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Web Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div>
+          <button className="heroButton">About</button>
+          <button className="heroButton">Experience</button>
+          <button className="heroButton">Skills</button>
+          <button className="heroButton">Projects</button>
+        </div>
+      </div>
+    </div>
+  );
 }
