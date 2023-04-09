94f78cd19c317c8e6dbd62e07cc016e17cbd2cd6
Martin Andersson <martin.andersson@me.com>
2023-04-09 22:00:56 +0200

    Skills section

---
+++ components/Skill.tsx
@@ -0,0 +1,14 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+type Props = {}
+
+function Skill({}: Props) {
+  return <div>
+      <motion.img
+
+      />
+    </div>
+}
+
+export default Skill
