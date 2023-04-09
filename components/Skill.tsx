516991d5d1b4996cd3220c61702303bafd5afd87
Martin Andersson <martin.andersson@me.com>
2023-04-09 22:57:33 +0200

    motion effects for skills component

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,14 +1,31 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 
-type Props = {}
+type Props = {
+    directionLeft?: boolean;
+}
 
-function Skill({}: Props) {
-  return <div>
+function Skill({ directionLeft }: Props) {
+  return (
+    <div className='group relative flex cursor-pointer'>
       <motion.img
-
+        initial={{
+            x: directionLeft ? -200 : 200,
+            opacity: 0,
+        }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x:0 }}
+        src='/typescript.svg'
+        className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32
+        xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out '
       />
+      <div>
+        <div>
+            <p> </p>
+        </div>
+      </div>
     </div>
+  )
 }
 
 export default Skill
