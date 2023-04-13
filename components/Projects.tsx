ae656c14d1547f76dcf8b3ee3bcca57a0e4f00eb
Martin Andersson <martin.andersson@me.com>
2023-04-13 15:34:10 +0200

    starting contact section

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import { motion } from 'framer-motion';
 type Props = {}
 
 function Projects({}: Props) {
   const projects = [1, 2, 3, 4, 5];
 
   return (
-    <div
-      className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{ opacity: 1 }}
+    transition={{ duration: 1.5 }}
+    className="h-screen relative flex overflow-hidden flex-col text-left md:flex-row
         max-w-full justify-evenly mx-auto items-center z-0">
 
         <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -21,7 +24,14 @@ function Projects({}: Props) {
             key={i}
             className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5
             items-center justify-center p-20 md:p-44 h-screen">
-              <img
+              <motion.img
+                initial={{
+                  y: -300,
+                  opacity: 0,
+                }}
+                transition={{ duration: 1.2 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                //viewport={{ once: true }}
                 src="/nextjs.svg"
                 alt=""
               />
@@ -47,7 +57,7 @@ function Projects({}: Props) {
 
       <div className="w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12"
       />
-    </div>
+    </motion.div>
   );
 }
 
