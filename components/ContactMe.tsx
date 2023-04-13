f45effff308cf754f5eb10f3dcca5a3073342088
Martin Andersson <martin.andersson@me.com>
2023-04-13 20:42:28 +0200

    addded form to contact page

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
+import { useForm, SubmitHandler } from "react-hook-form";
 
-type Props = {}
+
+type Inputs = {
+  name: string,
+  email: string,
+  subject: string,
+  message: string,
+};
+
+type Props = {};
 
 function ContactMe({}: Props) {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => console.log(formData);
+
   return (
     <div
       className="h-screen flex relative flex-col text-center md:text-left md:flex-row
@@ -12,15 +25,67 @@ function ContactMe({}: Props) {
         Contact
       </h3>
 
-      <div className='flex flex-col space-y-10'>
-        <h4 className='text-4xl font-semibold text-center'>
+      <div className="flex flex-col space-y-10">
+        <h4 className="text-4xl font-semibold text-center">
           I have got just what you need.{" "}
           <span className="underline decoration-[#F7AB0A]/50">Lets talk</span>
         </h4>
 
-        <div>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="text-[#F7AB0A] h-7 w-t animate-bounce" />
+            <p className="text-2xl">+46 73 588 01 88</p>
+          </div>
 
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="text-[#F7AB0A] h-7 w-t animate-pulse" />
+            <p className="text-2xl">martin.andersson@me.com</p>
+          </div>
+
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="text-[#F7AB0A] h-7 w-t" />
+            <p className="text-2xl">Gothenburg, Sweden</p>
+          </div>
         </div>
+
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
+          </div>
+
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
