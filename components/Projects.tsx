b2d5bbc14a4684835bad19ac0a91c7ad12832bad
Martin Andersson <martin.andersson@me.com>
2023-04-13 11:38:23 +0200

    fix

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -13,9 +13,9 @@ function Projects({}: Props) {
         Projects
       </h3>
 
-      <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x
+     {/*  <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x
       snap-mandatory z-20'>
-        {projects.map((project, i) => (
+        {projects.map((i) => (
           <div className='w-screen flex-shrink-0 snap-center flex flex-col
           space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
             <img
@@ -30,10 +30,10 @@ function Projects({}: Props) {
             </div>
             </div>
         ))}
-      </div>
+      </div> */}
 
-      <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
-    </div>
+{/*       <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
+ */}      </div>
   );
 }
 
