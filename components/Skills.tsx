61618d05d94d02a0f9c3f86747537c97910b945f
Martin Andersson <martin.andersson@me.com>
2023-04-22 15:02:37 +0200

    fixing skills section

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -13,9 +13,12 @@ function Skills({ skills }: Props) {
       className="flex relative flex-col text-center md:text-left xl:flex-row
       max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-12 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
+      <p className="absolute top-24 mt-20 tracking-[2px] text-gray-500 text-sm">
+        Hover over!
+      </p>
       <div className="pt-16 grid grid-cols-4 gap-5">
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
