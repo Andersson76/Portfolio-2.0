5a5804ddebdc64ce286a2212d5e47b7d634e56f8
Martin Andersson <martin.andersson@me.com>
2023-05-04 20:49:19 +0200

    arrange skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -14,22 +14,22 @@ function Skill({ skill, directionLeft }: Props) {
     <div className="group relative flex cursor-pointer">
       <motion.img
         initial={{
-          x: directionLeft ? -200 : 200,
+          x: directionLeft ? -74 : 74,
           opacity: 0,
         }}
         transition={{ duration: 1 }}
         whileInView={{ opacity: 1, x: 0 }}
-
         src={urlFor(skill?.image).url()}
-
-        className=" border border-gray-500 w-24 h-24 xl:w-32
-        xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out "
-        />
-      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300
-      ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32
-       z-0'>
-        <div className='flex items-center justify-center h-full'>
-          <p className='text-2xl font-bold flex text-center text-black opacity-100'>
+        className=" border border-gray-500 w-15 h-15 md:w-20 md:h-20 xl:w-24
+        xl:h-24 filter group-hover:grayscale transition duration-300 ease-in-out "
+      />
+      <div
+        className="absolute opacity-0 group-hover:opacity-80 transition duration-300
+      ease-in-out group-hover:bg-white h-15 w-15 md:w-20 md:h-20 xl:w-24 xl:h-24
+       z-0"
+      >
+        <div className="flex items-center justify-center h-full">
+          <p className="text-md font-bold flex text-center text-black opacity-100">
             {skill.name}
           </p>
         </div>
